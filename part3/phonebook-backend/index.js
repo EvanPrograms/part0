fd1966c847e8ec62784497b0ce5fda6aa334b3cf
Evan Perry <eperry2688@gmail.com>
2024-02-01 21:59:31 -0700

    3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -55,6 +57,17 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end(`Entry ${id} has been deleted`)
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  person.id = generateId()
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
