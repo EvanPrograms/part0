0de1ba36e6bff11ce680b599674ce8a03269354e
Evan Perry <eperry2688@gmail.com>
2024-02-06 00:16:13 -0700

    Add 3.10

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -99,7 +100,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
