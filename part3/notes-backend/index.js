f267e20efd5345e73c3655b912f7fc1b2f0e6d07
Evan Perry <eperry2688@gmail.com>
2024-02-09 01:30:25 -0700

    Updated notes for Error  Handling

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -4,9 +4,20 @@ const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
 
-app.use(express.json())
-app.use(cors())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
+
+
 
 // let notes = [
 //   {
@@ -35,6 +46,11 @@ app.get('/api/notes', (request, response) => {
       response.json(notes)
     })
   })
+  
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+  
 
 // app.get('/api/notes/:id', (request, response) => {
 //   const id = Number(request.params.id)
@@ -46,17 +62,40 @@ app.get('/api/notes', (request, response) => {
 //   }
 // })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+      response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
     response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -82,6 +121,20 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
