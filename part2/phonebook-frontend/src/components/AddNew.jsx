59f81a47b82d4c64ac1d597eaa5ea60d565951a7
Evan Perry <eperry2688@gmail.com>
2024-02-11 22:06:36 -0700

    Cleaned out comments and logs in frontend

--- part2/phonebook-frontend/src/components/AddNew.jsx
+++ part2/phonebook-frontend/src/components/AddNew.jsx
@@ -6,18 +6,15 @@ const AddNew = (props) => {
     const [newNumber, setNewNumber] = useState('')
   
     const handleNameChange = (event) => {
-      console.log(event.target.value)
       setNewName(event.target.value)
     }
   
     const handleNumberChange = (event) => {
-      console.log(event.target.value)
       setNewNumber(event.target.value)
     }
     const submitName = (event) => {
       event.preventDefault()
       const person = props.names.find(person => person.name === newName)
-      console.log('THIS IS THE PERSON', person)
       const nameObject = {
         name: newName,
         number: newNumber
@@ -76,7 +73,6 @@ const AddNew = (props) => {
             
           })
           .catch(error => {
-            console.log('THIS IS THE ERROR', error.response.data.error)
             props.setAddMessage({
               message: error.response.data.error,
               alert: false
@@ -93,80 +89,6 @@ const AddNew = (props) => {
         setNewNumber('')
       } 
 
-
-
-    //   if (props.names.some(person => person.name === nameObject.name) === false) {
-    //     props.setpersons(props.names.concat(nameObject))
-    //     console.log("WE FRIGGIN ADDED THIS GUY")
-    //     props.setAddMessage({
-    //       message: `Added ${nameObject.name}`,
-    //       alert: true
-    //     })
-    //     setTimeout(() => {
-    //       props.setAddMessage({
-    //         message: null,
-    //         alert: false
-    //       })
-    //     }, 5000)
-    //     personService
-    //       .create(nameObject)
-    //       .then(response => {response.data; props.setRequestData(new Date())})
-    //       .catch(error => {
-    //         console.log('THIS IS THE ERROR', error.response.data.error)
-    //         props.setAddMessage({
-    //           message: error.response.data.error,
-    //           alert: false
-    //         })
-    //         setTimeout(() => {
-    //           props.setAddMessage({
-    //             message: null,
-    //             alert: false
-    //           })
-    //         }, 5000)
-    //       })
-    //     console.log("NAME NOT IN DATABASE")
-    //   } else {
-    //     const identity = props.names.find(person => person.name === nameObject.name)
-    //     const confirmation = window.confirm(
-    //       `${newName} is already in the phonebook, would you like to replace the old number with a new one?`
-    //       )
-    //     if (confirmation) {
-    //       personService
-    //       .update(identity.id, nameObject)
-    //       .catch(error => {
-    //         props.setAddMessage({
-    //           message: `Information of ${nameObject.name} has already been removed from the server`,
-    //           alert: false
-    //         })
-    //         setTimeout(() => {
-    //           props.setAddMessage({
-    //             message: null,
-    //             alert: false
-    //           })
-    //         }, 5000)
-    //       })  
-    //       .then(response => {response.data; 
-    //         props.setRequestData(new Date());
-    //         props.setAddMessage({
-    //           message: `Updated ${nameObject.name}'s number`,
-    //           alert: true
-    //         })
-    //         setTimeout(() => {
-    //           props.setAddMessage({
-    //             message: null,
-    //             alert: false
-    //           })
-    //         }, 5000)
-            
-
-    //       })
-    //     }
-    //   }
-    //   setNewName('')
-    //   setNewNumber('')
-    //   console.log(props.names, newName)
-    // }
-    
     return (
       <div>
         <form onSubmit={submitName}>
