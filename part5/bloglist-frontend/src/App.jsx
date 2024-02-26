efb3f8332dba8a9d5667d345872d030e99a690ac
Evan Perry <eperry2688@gmail.com>
2024-02-26 15:28:47 -0700

    5.1 Blog List Frontend, login/blog

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with ', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      const userBlogs = blogs.filter(blog => blog.user.username === user.username)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setBlogs(userBlogs)
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>   
+    </div>   
+  )
+
+  const blogList = () => {
+    return (
+    <div>
+    <h2>blogs</h2>
+    <p>
+      {user.name} logged in
+    </p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+    )
+  }
+
+  return (
+    <div>
+      {user === null ? loginForm() : blogList() }
+    </div>
+  )
+}
+
+export default App
