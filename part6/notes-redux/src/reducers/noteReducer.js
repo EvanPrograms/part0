62667106aad4b31b925b81b7da2c534c218ec21a
Evan Perry <eperry2688@gmail.com>
2024-04-06 23:55:52 -0600

    6C First part Notes

--- part6/notes-redux/src/reducers/noteReducer.js
+++ part6/notes-redux/src/reducers/noteReducer.js
@@ -18,15 +18,10 @@ const generateId = () =>
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        important: false,
-        id: generateId(),
-      })
+      state.push(action.payload)
     },
     toggleImportanceOf(state, action) {
       const id = action.payload
@@ -39,47 +34,15 @@ const noteSlice = createSlice({
       console.log('reducer toggler clog', current(state))
 
       return state.map(note => note.id !== id ? note : changedNote)
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
+    setNotes(state, action) {
+      return action.payload
     }
   }
 })
 
-// const noteReducer = (state = initialState, action) => {
-//   switch(action.type) {
-//     case 'NEW_NOTE':
-//       return [...state, action.payload]
-//     case 'TOGGLE_IMPORTANCE': {
-//       const id = action.payload.id
-//       const noteToChange = state.find(n => n.id === id)
-//       const changedNote = { 
-//         ...noteToChange, 
-//         important: !noteToChange.important 
-//       }
-//       return state.map(note =>
-//         note.id !== id ? note : changedNote 
-//       )
-//      }
-//     default:
-//       return state
-//   }
-// }
-
-// export const createNote = (content) => {
-//   return {
-//     type: 'NEW_NOTE',
-//     payload: {
-//       content,
-//       important: false,
-//       id: generateId()
-//     }
-//   }
-// }
-
-// export const toggleImportanceOf = (id) => {
-//   return {
-//     type: 'TOGGLE_IMPORTANCE',
-//     payload: { id }
-//   }
-// }
-
-export const { createNote, toggleImportanceOf } = noteSlice.actions
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
 export default noteSlice.reducer
