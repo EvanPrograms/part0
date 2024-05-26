5936cd766fa03f0434a7b4348bc36c3db7d5b908
Evan Perry <eperry2688@gmail.com>
2024-05-26 13:19:15 -0600

    8.6 Addbook graphQL

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -107,6 +108,7 @@ const typeDefs = `
 
   type Author {
     name: String!
+    born: Int
     bookCount: Int!
   }
 
@@ -116,6 +118,15 @@ const typeDefs = `
     authorCount: Int
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -145,6 +156,19 @@ const resolvers = {
           bookCount: authorbooks.length
         }
       })
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      let author = authors.find(author => author.name === args.author)
+
+      if (!author) {
+        author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
     }
   }
 }
