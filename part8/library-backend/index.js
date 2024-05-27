1b2aeed1760287de9b865df06db48061243b1543
Evan Perry <eperry2688@gmail.com>
2024-05-26 19:32:59 -0600

    8.7 editAuthor GraphQL

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -127,6 +127,10 @@ const typeDefs = `
       published: Int
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -169,6 +173,16 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (roots, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
