d9d8195929981d00107d44b5cad47056504c5974
Evan Perry <eperry2688@gmail.com>
2024-06-06 18:48:24 -0600

    8.17 Frontend Fix, backend Fix

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -184,17 +184,47 @@ const resolvers = {
     // authorCount: () => authors.length,
     authorCount: async () => Author.collection.countDocuments(),
     // allBooks: (_, { author, genre }) => {
-    allBooks: (root, args) => {
-      if (args.author && args.genre) {
-        const authorBooks = books.filter(book => book.author === args.author)
-        return authorBooks.filter(book => book.genres.includes(args.genre))
-      } else if (args.author) {
-        return books.filter(book => book.author === args.author)
-      } else if (args.genre) {
-        return books.filter(book => book.genres.includes(args.genre))
-      } else {
-        return books
-      }
+    allBooks: async (root, args) => {
+      const authorFilter = args.author
+        ? { author: await Author.findOne({ name: args.author }) }
+        : {}
+      const genreFilter = args.genre ? { genres: { $in: args.genre } } : {}
+      return Book.find({ ...authorFilter, ...genreFilter }).populate('author')
+      // if (args.author && args.genre) {
+      //   const authorBooks = books.filter(book => book.author === args.author)
+      //   return authorBooks.filter(book => book.genres.includes(args.genre))
+      // } else if (args.author) {
+      //   return books.filter(book => book.author === args.author)
+      // } else if (args.genre) {
+      //   return books.filter(book => book.genres.includes(args.genre))
+      // } else {
+      //   return books
+      // }
+
+      // try {
+      //   let filteredBooks = books
+
+      //   if (args.author) {
+      //     filteredBooks = filteredBooks.filter(book => book.author === args.author)
+      //   }
+
+      //   if (args.genre) {
+      //     filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre))
+      //   }
+
+      //   // return filteredBooks.map(book => ({
+      //   //   ...book,
+      //   //   author: {name: book.author}
+      //   // }))
+      //   return Book.find({})
+      // } catch (error) {
+      //   throw new GraphQLError('Failed to fetch books', {
+      //     extensions: {
+      //       code: 'INTERNAL_SERVER_ERROR',
+      //       error
+      //     }
+      //   })
+      // }
     },
     allAuthors: async () => {
       // return authors.map(author => {
@@ -211,6 +241,32 @@ const resolvers = {
       return context.currentUser
     }
   },
+  Author: {
+    bookCount: async (root) => {
+      return Book.collection.countDocuments({ author: root._id })
+    },
+  },
+  // Book: {
+  //   author: async (root, args, context) => {
+  //     try {
+  //       // const author = await Author.findById(root.author)
+  //       const author = await Author.findOne({ name: root.author })
+  //       console.log('backend author', author)
+  //       if (!author) {
+  //         throw new Error(`Author '${root.author}' is not found`)
+  //       }
+  //       return author
+  //     } catch (error) {
+  //       // console.error('Error fetching author:', error)
+  //       throw new GraphQLError('Failed to fetch author', {
+  //         extensions: {
+  //           code: 'BAD_USER_INPUT',
+  //           invalidArgs: root.author
+  //         }
+  //       })
+  //     }
+  //   }
+  // },
   Mutation: {
     addBook: async (root, args) => {
       try {
