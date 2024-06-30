1d1ad94186a0175260f46a4663944fd911858fbe
Evan Perry <eperry2688@gmail.com>
2024-06-29 22:34:52 -0600

    9.10 Patientor backend, step3

---
+++ part9/flight-diaries/src/services/diaryService.ts
@@ -0,0 +1,29 @@
+import diaries from '../../data/entries';
+
+import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+
+// const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+const getNonSensitiveEntries = 
+  (): NonSensitiveDiaryEntry[] => {
+    return diaries.map(({ id, date, weather, visibility }) => ({
+      id,
+      date,
+      weather,
+      visibility,
+    }));
+  };
+
+export default {
+  getEntries,
+  addDiary,
+  getNonSensitiveEntries
+};
