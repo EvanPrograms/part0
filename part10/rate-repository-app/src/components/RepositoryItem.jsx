45ba3ce210fb512916f1b078adc08b3e81ade1ac
Evan Perry <eperry2688@gmail.com>
2024-07-12 04:10:10 -0600

    10.3

---
+++ part10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,26 @@
+import { FlatList, View, StyleSheet, Text } from 'react-native';
+
+
+const RepositoryItem = ({ 
+  fullName, 
+  description, 
+  language, 
+  stargazersCount,
+  forkscount,
+  reviewCount,
+  ratingAverage
+  }) => {
+  return (
+    <View>
+      <Text >Full name: {fullName}</Text>
+      <Text >Description: {description}</Text>
+      <Text >Language: {language}</Text>
+      <Text >Stars: {stargazersCount}</Text>
+      <Text >Forks: {forkscount}</Text>
+      <Text >Reviews: {reviewCount}</Text>
+      <Text >Rating: {ratingAverage}</Text>
+    </View>
+  );
+}
+
+export default RepositoryItem;
